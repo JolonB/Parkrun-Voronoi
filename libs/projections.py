f8a2996ac87c48d492da7234df126c3ab566df0d
Jolon <jolon.behrent@gmail.com>
2025-03-02 22:02:22 +1300

    Update map drawing

--- libs/projections.py
+++ libs/projections.py
@@ -1,6 +1,5 @@
+import numpy as np
 from pyproj import CRS, Transformer
-
-EQUATOR
 
 latlon = CRS.from_string("EPSG:4326")
 
@@ -10,19 +9,40 @@ mercator = CRS.from_string("EPSG:3857")
 ecef = CRS.from_string("EPSG:4978")
 # ecef = CRS.from_string("EPSG:6500")
 
+EARTH_RADIUS = 1.0 * mercator.ellipsoid.semi_major_metre  # 6378137.0 metres
+EARTH_CIRCUMFERENCE = 2 * np.pi * EARTH_RADIUS
+
 tf_latlon_to_mercator = Transformer.from_crs(latlon, mercator)
 tf_latlon_to_ecef = Transformer.from_crs(latlon, ecef)
 tf_ecef_to_latlon = Transformer.from_crs(ecef, latlon)
 tf_ecef_to_mercator = Transformer.from_crs(ecef, mercator)
 
 def latlon_to_ecef(latitude, longitude):
-    return tf_latlon_to_ecef.transform(latitude, longitude, 0)
+    points = tf_latlon_to_ecef.transform(latitude, longitude, 0)
+    return (points[0]/EARTH_RADIUS, points[1]/EARTH_RADIUS, points[2]/EARTH_RADIUS)
 
 def ecef_to_latlon(x, y, z):
-    return tf_latlon_to_ecef.transform(x, y, z)[:2]
+    x = x * EARTH_RADIUS
+    y = y * EARTH_RADIUS
+    z = z * EARTH_RADIUS
+    return tf_ecef_to_latlon.transform(x, y, z)[:2]
 
 def latlon_to_mercator(latitude, longitude):
     return tf_latlon_to_mercator.transform(latitude, longitude)
 
 def ecef_to_mercator(x, y, z):
     return tf_ecef_to_mercator.transform(x, y, z)[:2]
+
+def mercator_to_array(merc_x, merc_y, map_width, map_height):
+    merc_x = (merc_x / EARTH_CIRCUMFERENCE + 0.5) * map_width
+    merc_y = (0.5 - merc_y / EARTH_CIRCUMFERENCE) * map_height
+    return int(round(merc_x)), int(round(merc_y))
+
+def pixels_between_mercator_points(merc_x1, merc_y1, merc_x2, merc_y2, map_width, map_height):
+    """
+    The number of pixels between two points on a mercator map, following the great circle path
+
+    Just return max(map_width,map_height) / 2 for now, as I don't know how to actually calculate this.
+    There should never be two points more than this distance apart (I think).
+    """
+    return int(round(max(map_width, map_height) / 2))
--- sphere_test.py
+++ sphere_test.py
@@ -8,53 +8,61 @@ from scipy.spatial import SphericalVoronoi, geometric_slerp
 
 import libs.parkrun_api.parkrun_api as parkrun
 import libs.coordinates as coords
+import libs.projections as proj
 
+GENERATE_SPHERE_PLOT = False
+
+print("Getting parkrun events")
 events: List[parkrun.Event] = parkrun.Event.GetAllEvents()
 adult_events = [event for event in events if event.seriesId == 1]
 junior_events = [event for event in events if event.seriesId == 2]
 
-points = [coords.latlon_to_ecef(event.latitude, event.longitude) for event in adult_events]
-points_norm = np.array([point / np.linalg.norm(point) for point in points])  # TODO probably doesn't need to be normalised
-# print(points_norm)
 
+points = [proj.latlon_to_ecef(event.latitude, event.longitude) for event in adult_events]
+points_norm = np.array([point / np.linalg.norm(point) for point in points])  # TODO probably doesn't need to be normalised
+
+print("Calculating spherical voronoi")
 radius = 1
 center = np.array([0, 0, 0])
 sv = SphericalVoronoi(points_norm, radius, center)
 sv.sort_vertices_of_regions()
 t_vals = np.linspace(0, 1, 2000)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-u = np.linspace(0, 2 * np.pi, 100)
-v = np.linspace(0, np.pi, 100)
-x = np.outer(np.cos(u), np.sin(v))
-y = np.outer(np.sin(u), np.sin(v))
-z = np.outer(np.ones(np.size(u)), np.cos(v))
-ax.plot_surface(x, y, z, color='y', alpha=0.1)
-ax.scatter(points_norm[:, 0], points_norm[:, 1], points_norm[:, 2], c='b')
-ax.scatter(sv.vertices[:, 0], sv.vertices[:, 1], sv.vertices[:, 2],
-                   c='g', alpha=0.2)
-for region in sv.regions:
-    n = len(region)
-    for i in range(n):
-        start = sv.vertices[region][i]
-        end = sv.vertices[region][(i + 1) % n]
-        result = geometric_slerp(start, end, t_vals)
-        ax.plot(result[..., 0],
-                result[..., 1],
-                result[..., 2],
-                c='k')
-# breakpoint()
-# try:
-#     os.mkdir('output')
-# except FileExistsError:
-#     pass
+if GENERATE_SPHERE_PLOT:
+    print("Generating sphere plot")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = np.outer(np.cos(u), np.sin(v))*.95
+    y = np.outer(np.sin(u), np.sin(v))*.95
+    z = np.outer(np.ones(np.size(u)), np.cos(v))*.95
+    ax.plot_surface(x, y, z, color='y', alpha=0.7)
+    ax.scatter(points_norm[:, 0], points_norm[:, 1], points_norm[:, 2], c='b')
+    ax.scatter(sv.vertices[:, 0], sv.vertices[:, 1], sv.vertices[:, 2],
+                    c='g', alpha=0.05)
+    for region in sv.regions:
+        n = len(region)
+        for i in range(n):
+            start = sv.vertices[region][i]
+            end = sv.vertices[region][(i + 1) % n]
+            result = geometric_slerp(start, end, t_vals)
+            ax.plot(result[..., 0],
+                    result[..., 1],
+                    result[..., 2],
+                    c='k')
+    try:
+        os.mkdir('output')
+    except FileExistsError:
+        pass
 
-# for i in range(36):
-#     ax.azim = 10*i
-#     ax.elev = 10
-#     # fig.set_size_inches(4, 4)
-#     plt.savefig('output/foo_{}.png'.format(i))
+    STEP = 20
+    for i in range(360 // STEP):
+        print("Drawing {}".format(i))
+        ax.azim = STEP*i
+        ax.elev = 10
+        fig.set_size_inches(4, 4)
+        plt.savefig('output/foo_{}.png'.format(i))
 
 ########################################
 # FLATTEN IMAGE
@@ -66,75 +74,114 @@ class Shape:
     POINT = 3
 
 def add_latlon_to_map(latitude: float, longitude: float, img: np.ndarray, shape: Shape):
-    merc_x, merc_y = coords.latlon_to_mercator(latitude, longitude, MAP_WIDTH, MAP_HEIGHT)
-    # if merc_y < 0:
-    #     return
-    merc_x = int(round(merc_x))
-    merc_y = int(round(merc_y))
+    # Skip points if above or below mercator projection
+    if latitude < -85 or latitude > 85:
+        return
+    merc_x, merc_y = proj.latlon_to_mercator(latitude, longitude)
+    merc_x, merc_y = proj.mercator_to_array(merc_x, merc_y, MAP_WIDTH, MAP_HEIGHT)
     img[merc_y, merc_x] = 0
+    # Draw rest of shape, being aware of the bounds of the image
     if shape == Shape.PLUS:
-        img[merc_y-1, merc_x] = 0
-        img[merc_y+1, merc_x] = 0
-        img[merc_y, merc_x-1] = 0
-        img[merc_y, merc_x+1] = 0
+        try:
+            img[merc_y-1, merc_x] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y+1, merc_x] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y, merc_x-1] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y, merc_x+1] = 0
+        except IndexError:
+            pass
     elif shape == Shape.CROSS:
-        img[merc_y-1, merc_x-1] = 0
-        img[merc_y-1, merc_x+1] = 0
-        img[merc_y+1, merc_x-1] = 0
-        img[merc_y+1, merc_x+1] = 0
+        try:
+            img[merc_y-1, merc_x-1] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y-1, merc_x+1] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y+1, merc_x-1] = 0
+        except IndexError:
+            pass
+        try:
+            img[merc_y+1, merc_x+1] = 0
+        except IndexError:
+            pass
     elif shape == Shape.POINT:
         pass  # no more needs to be drawn
 
-
+print("Converting voronoi vertices to latlon")
 vertices_latlon = np.zeros((len(sv.vertices), 2))
 for index, vertex in enumerate(sv.vertices):
-    vertices_latlon[index] = coords.ecef_to_latlon(*vertex)
+    vertices_latlon[index] = proj.ecef_to_latlon(*vertex)
     if round(vertices_latlon[index][0],2) == 88.58:
         breakpoint()
     elif round(vertices_latlon[index][1], 2) == 42.40:
         breakpoint()
 
-MAP_WIDTH = 2000
-MAP_HEIGHT = 1000
-img_buffer = np.ones((MAP_HEIGHT+1, MAP_WIDTH+1), dtype=np.uint8) * 255
+MAP_WIDTH = 2044
+MAP_HEIGHT = 2044
+img_buffer = np.ones((MAP_HEIGHT, MAP_WIDTH), dtype=np.uint8) * 255
 
-# for region in sv.regions[:500]:
-#     n = len(region)
-#     for i in range(n):
-#         start = sv.vertices[region][i]
-#         end = sv.vertices[region][(i + 1) % n]
-#         try:
-#             edge_ecef = geometric_slerp(start, end, t_vals)
-#         except ValueError as e:
-#             print(e)
-#             print((start, end))
-#         edge_latlon = []
-#         for point_ecef in edge_ecef:
-#             point_latlon = coords.ecef_to_latlon(*point_ecef)
-#             try:
-#                 if np.all(np.round(edge_latlon[-1], 2) == np.round(point_latlon, 2)):
-#                     continue
-#             except IndexError as e:
-#                 pass
-#             edge_latlon.append(point_latlon)
-#         # edge_latlon = [coords.ecef_to_latlon(*point) for point in edge_ecef]
-#         for point in edge_latlon:
-#             try:
-#                 add_latlon_to_map(point[0], point[1], img_buffer, Shape.POINT)
-#             except Exception as e:
-#                 print(e)
-#                 print((start, end))
-#                 print((edge_latlon[0], edge_latlon[-1]))
-#                 print(point)
+print("Adding edges to map")
+for region in sv.regions[:100]:
+    n = len(region)
+    for i in range(n):
+        start = sv.vertices[region][i]
+        end = sv.vertices[region][(i + 1) % n]
+        # t_vals = np.linspace(0, 1, proj.pixels_between_mercator_points(*start, *end, MAP_WIDTH, MAP_HEIGHT))
+        try:
+            edge_ecef = geometric_slerp(start, end, t_vals)
+        except ValueError as e:
+            print(e)
+            print((start, end))
+        edge_latlon = []
+        for point_ecef in edge_ecef:
+            point_latlon = proj.ecef_to_latlon(*point_ecef)
+            try:
+                if np.all(np.round(edge_latlon[-1], 2) == np.round(point_latlon, 2)):
+                    continue
+            except IndexError as e:
+                pass
+            edge_latlon.append(point_latlon)
+        # breakpoint()  # TODO why aren't these coordinates being put in the correct location?
+        ## TODO perhaps something to do with ecef_to_latlon
+        # edge_latlon = [coords.ecef_to_latlon(*point) for point in edge_ecef]
+        for point in edge_latlon:
+            try:
+                add_latlon_to_map(point[0], point[1], img_buffer, Shape.POINT)
+                # print("Drawing {}".format(point))
+            except Exception as e:
+                print(e)
+                print((start, end))
+                print((edge_latlon[0], edge_latlon[-1]))
+                print(point)
 
 
 for vertex in vertices_latlon:
     add_latlon_to_map(vertex[0], vertex[1], img_buffer, Shape.PLUS)
 
-for event in events:
+print("Adding events to map")
+for event in adult_events:
     add_latlon_to_map(event.latitude, event.longitude, img_buffer, Shape.CROSS)
 
-img = Image.fromarray(img_buffer)
+background = Image.open("assets/Mercator_projection_Square.jpg")
+# Apply the mask
+background = background.convert("L")
+background = np.array(background)
+background[img_buffer == 0] = 0
+img = Image.fromarray(background)
 img.save("map.png")
+
+# img = Image.fromarray(img_buffer)
+# img.save("map.png")
 
 # breakpoint()
